5c158a3a012690e5a13117e200eea9bc9f595529
xiaomage118 <1612823478@qq.com>
2022-06-15 17:23:01 +0800

    首次提交

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
+def juejin_auto():
+    herder = {
+        'referer': 'https://juejin.cn/',
+        'origin': 'https://juejin.cn',
+        'content-type': 'application/json',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+        'cookie': 'cookie: _ga=GA1.2.1811657961.1642139615; MONITOR_WEB_ID=4e97a8a5-642f-4940-8938-678c61b529cd; __tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25227052935898246088232%2522%252C%2522web_id%2522%253A%25227052935898246088232%2522%252C%2522timestamp%2522%253A1642555869527%257D; passport_csrf_token=ed96303b1b77b4d80a754ae29983b677; passport_csrf_token_default=ed96303b1b77b4d80a754ae29983b677; n_mh=CIUBtfW4A8DOGakAhyYLrNIn199dcNF_dDOoGJQq1B8; sid_guard=6d2ac110446bbd783d13744383bafc65%7C1652665064%7C31536000%7CTue%2C+16-May-2023+01%3A37%3A44+GMT; uid_tt=eb8528e3b7c47ccfe1c780636dc88052; uid_tt_ss=eb8528e3b7c47ccfe1c780636dc88052; sid_tt=6d2ac110446bbd783d13744383bafc65; sessionid=6d2ac110446bbd783d13744383bafc65; sessionid_ss=6d2ac110446bbd783d13744383bafc65; sid_ucp_v1=1.0.0-KDY3NjU5ZTZiN2NkYWNiZWNiZDFmNjZhNWE5NDgyZWZjZjBiOTNhMDAKFwjX6vDA_fXlBhDo1YaUBhiwFDgCQPEHGgJsZiIgNmQyYWMxMTA0NDZiYmQ3ODNkMTM3NDQzODNiYWZjNjU; ssid_ucp_v1=1.0.0-KDY3NjU5ZTZiN2NkYWNiZWNiZDFmNjZhNWE5NDgyZWZjZjBiOTNhMDAKFwjX6vDA_fXlBhDo1YaUBhiwFDgCQPEHGgJsZiIgNmQyYWMxMTA0NDZiYmQ3ODNkMTM3NDQzODNiYWZjNjU; _tea_utm_cache_2608={%22utm_source%22:%22gold_browser_extension%22}; _gid=GA1.2.1364460869.1655083088'
+    }
+    url = 'https://api.juejin.cn/content_api/v1/short_msg/publish?aid=2608&uuid=7052935898246088232'
+    params = {
+        'content': 'xdm,摸鱼的一天结束啦',
+        'topic_id': '6824710203301167112',
+        'sync_to_org': False
+    }
+    reqs = requests.post(url=url, headers=herder, json=params)
+    print(reqs.json())
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    juejin_auto()
