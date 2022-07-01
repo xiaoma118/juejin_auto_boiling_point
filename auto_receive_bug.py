9c78504d6f495830e2e6f5d120ac013001abf2f4
xiaomage118 <1612823478@qq.com>
2022-07-01 11:01:07 +0800

    添加掘自动领取bugfix

---
+++ auto_receive_bug.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import requests
+
+from message_push import push_content
+
+herder = {
+    'referer': 'https://juejin.cn/',
+    'origin': 'https://juejin.cn',
+    'content-type': 'application/json',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+    'cookie': os.environ.get('cookie')
+    # 'cookie': sys.argv[1]
+}
+
+
+def get_bug_list():
+    """
+    获取bug数量列表
+    :return:
+    """
+
+    print('获取待领取的bug列表')
+    url = 'https://api.juejin.cn/user_api/v1/bugfix/not_collect?aid=2608&uuid=7052935898246088232'
+    params = {}
+    resp = requests.post(url=url, headers=herder, json=params)
+    data = resp.json()
+    print(data)
+
+    if data['err_msg'] == 'success':
+        auto_receive_bug(data['data'])
+    else:
+        print('获取失败，请检查cookie')
+
+
+def auto_receive_bug(data):
+    """
+    自动领取掘金BUG
+    :return:
+    """
+    if not data:
+        print('没有可领取的bug')
+        user_own_bug = get_user_own_bug()
+        content = '掘金消息：\n本次操作领取了%s个bug\n当前bug数量：%s' % (len(data), user_own_bug)
+        push_content(content)
+        return
+    print('开始领取bug')
+    url = 'https://api.juejin.cn/user_api/v1/bugfix/collect?aid=2608&uuid=7052935898246088232'
+
+    for bug in data:
+        params = {
+            'bug_time': bug['bug_time'],
+            'bug_type': bug['bug_type']
+        }
+        resp = requests.post(url, json=params, headers=herder)
+        print('领取成功', resp.json()['success'])
+    user_own_bug = get_user_own_bug()
+    content = '掘金消息：\n本次操作领取了%s个bug\n当前bug数量：%s' % (len(data), user_own_bug)
+    push_content(content)
+
+
+def get_user_own_bug():
+    """
+    获取当前bug数量
+    :return:
+    """
+    # 先获取competition_id
+    params = {
+        'competition_id': ''
+    }
+    url_bugfix = 'https://api.juejin.cn/user_api/v1/bugfix/competition?aid=2608&uuid=7052935898246088232'
+    resp = requests.post(url=url_bugfix, headers=herder, json=params)
+    params['competition_id'] = resp.json()['data']['competition_id']
+    # 使用competition_id获取 账户bug数量
+    url = 'https://api.juejin.cn/user_api/v1/bugfix/user?aid=2608&uuid=7052935898246088232'
+    response = requests.post(url=url, headers=herder, json=params)
+    data = response.json()
+    if data['err_msg'] == 'success':
+        user_own_bug = data['data']['user_own_bug']
+        return user_own_bug
+    return False
+
+
+if __name__ == '__main__':
+    get_bug_list()
---
+++ message_push.py
@@ -0,0 +1,61 @@
+import sys
+
+import requests
+
+
+def get_access_token():
+    """
+    获取access_token
+    :return:
+    """
+    corpid = sys.argv[2]
+    corpsecret = sys.argv[3]
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s' % (corpid, corpsecret)
+    resp = requests.get(url=url)
+    return resp.json()['access_token']
+
+
+def push_content(content='消息推送测试'):
+    """
+    企业微信应用消息推送
+    :return:
+    """
+    access_token = get_access_token()
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s' % access_token
+    params = {
+        "touser": "",
+        "toparty": "1",
+        "totag": "",
+        "msgtype": "text",
+        "agentid": int(sys.argv[4]),
+        "text": {
+            "content": content
+        },
+        "safe": 0,
+        "enable_id_trans": 0,
+        "enable_duplicate_check": 0
+    }
+    resp = requests.post(url=url, json=params)
+    print(resp.json())
+
+
+def webhooks():
+    """
+    企业微信机器人消息推送
+    :return:
+    """
+    url = sys.argv[5]
+    content = {
+        "msgtype": "text",
+        "text": {
+            "content": "广州今日天气：29度，大部分多云，降雨概率：60%",
+            "mentioned_list": ["@all"],
+            "mentioned_mobile_list": ["@all"]
+        }
+    }
+    resp = requests.post(url=url, json=content)
+    print(resp.json())
+
+
+if __name__ == '__main__':
+    push_content()
